abf0bdc63b0234d4f1d67dd7f2e353edf7f442d7
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-06-05 01:30:15 +0000

    oppy day

---
+++ eieio.js
@@ -0,0 +1,3 @@
+let classArray = ["car 1", "car 2", "car 3", "car 4", "car 5"];
+let choice = prompt("pick a number between 0 and 4 zlunc👌👌👌👌");
+alert("person " + choice + " is " + classArray[choice]);
