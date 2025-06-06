f7c45c46d7a9b6f95b397b90a4b13d87bb15d634
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-06-06 02:31:19 +0000

    Jun 6, 2025, 2:31 AM

--- eieio.js
+++ eieio.js
@@ -1,4 +1,4 @@
-console.log("hello zlunc 2")
+console.log("IONWANNA do ts🥷🔫")
 let classArray = ["car 1", "car 2", "car 3", "car 4", "car 5"];
 let choice = prompt("pick a car between 0 and 4");
 alert("person " + choice + " is " + classArray[choice]);
