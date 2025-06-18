227ac557756a2c8e3df2fd7419083dd92422936d
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-06-18 00:18:44 +0000

    Jun 18, 2025, 12:18 AM

--- eieio.js
+++ eieio.js
@@ -1,32 +1,3 @@
-let getMeColor;
-let getuserName; 
-let abcdef;
-let ageidk;
-
-function start() {
-getMeColor = getFavColor()
-getuserName = getUserName()
-ageidk = askUserAge()
-
-}
-
-function getFavColor() {
-    userFavColor = prompt("What is your favourite color?");
-    alert("Your favourite color is: " + userFavColor);
-};
-
-
-function getUserName() {
-    getUserName = prompt("What's your name?")
-    alert("Welcome " + getUserName);
-};
-
-
-function askUserAge() {
-    userAge = prompt("How old are you?");
-    alert("Welcome " + userAge + " year old👋🙋‍♂️🙋‍♂️🙋‍♂️");
-};
-
 let classArray = ["Fiat 500", "Corvette ZR1", "Ford Focus", "Nissan Cube", "Toyota Prius"];
-let choice = prompt("pick a car between 0 and 4");
+let choice = prompt("How many seats do you want in your car");
 alert("car " + choice + " is " + classArray[choice]);
